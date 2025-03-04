7e74f7d47a9d97e319442735bd44dde3e319efa0
trueberryless <trueberryless+github@gmail.com>
2025-03-04 17:26:17 +0100

    add images and remove h2 (because in my repos these are repetitions of the versionNum

--- src/libs/rehype.ts
+++ src/libs/rehype.ts
@@ -2,6 +2,7 @@ import { unified } from "unified";
 import rehypeParse from "rehype-parse";
 import rehypeStringify from "rehype-stringify";
 import expressiveCode from "rehype-expressive-code";
+import { visit } from "unist-util-visit";
 
 export async function addExpressiveCode(html) {
   const result = await unified()
@@ -11,4 +12,27 @@ export async function addExpressiveCode(html) {
     .process(html);
 
   return result.toString();
+}
+
+export async function removeH1s(html) {
+  const result = await unified()
+    .use(rehypeParse, { fragment: true }) // Parse as HTML fragment
+    .use(myRehypePluginToRemoveHeadings1) // Add syntax highlighting
+    .use(rehypeStringify) // Convert back to HTML
+    .process(html);
+
+  return result.toString();
+}
+
+function myRehypePluginToRemoveHeadings1() {
+  /**
+   * @param {Root} tree
+   */
+  return function (tree: any) {
+    visit(tree, "element", function (node, index, parent) {
+      if (["h2"].includes(node.tagName)) {
+        parent.children.splice(index, 1);
+      }
+    });
+  };
 }
