47a913755009f203b6a0ed96a9321381de9f1a01
trueberryless <trueberryless+github@gmail.com>
2025-03-05 12:02:28 +0100

    up-to-date code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,32 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import markdownRemark from "@studiocms/markdown-remark";
+import expressiveCode from "rehype-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   image: {
     domains: ["trueberryless.org"],
+  },
+  markdown: {
+    rehypePlugins: [expressiveCode],
+  },
+  integrations: [
+    markdownRemark({
+      markdownExtended: {
+        autoLinkHeadings: true,
+        sanitize: {
+          dropElements: ["h2"],
+          dropAttributes: {
+            class: ["pre"],
+            style: ["pre"],
+          },
+        },
+      },
+    }),
+  ],
+  experimental: {
+    serializeConfig: true,
   },
 });
