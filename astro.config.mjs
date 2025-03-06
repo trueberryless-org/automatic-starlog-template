8f2d126098726aaeb3414fe308051d83b282e2c2
trueberryless <trueberryless+github@gmail.com>
2025-03-06 22:47:06 +0100

    add image domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import expressiveCode from "rehype-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
+  image: {
+    domains: ["release-image-generator.trueberryless.org"],
+  },
 });
